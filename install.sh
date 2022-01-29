b1c2a9b4a7d867a6c8dc46a72ff1b3fdccdc5edd
Junichi Sato <sat0b.3ee@gmail.com>
2022-01-29 16:03:19 +0900

    Update configs

--- install.sh
+++ install.sh
@@ -4,6 +4,3 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            -s https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# completion
-eval (gh completion -s fish| source)
-
--- ln.sh
+++ ln.sh
@@ -3,10 +3,6 @@
 # tmux
 ln -sf "$(pwd)/tmux.conf" "$HOME/.tmux.conf"
 
-# fish
-mkdir -p ~/.config/fish/
-ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
-
 # vim
 ln -sf "$(pwd)/vimrc" "$HOME/.vimrc"
 mkdir -p ~/.vim/backup ~/.vim/undo
@@ -14,10 +10,6 @@ mkdir -p ~/.vim/backup ~/.vim/undo
 # nvim
 mkdir -p ~/.config/nvim/
 ln -sf $(pwd)/vimrc ~/.config/nvim/init.vim
-ln -sf $(pwd)/vim/memolist_template/ ~/.config/nvim/
-
-# ideavim
-ln -sf "$(pwd/ideavimrc)" "$HOME/.ideavimrc"
 
 # git
 ln -sf $(pwd)/.gitconfig ~/.gitconfig

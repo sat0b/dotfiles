80a49a662614eabcd7d9a5865d31296eea8b232b
Junichi Sato <sat0b.3ee@gmail.com>
2022-02-07 21:10:32 +0900

    Update neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,4 @@ require('plugins')
 vim.cmd[[autocmd BufWritePost plugins.lua PackerCompile]]
 
 vim.cmd("set nu")
+vim.cmd("set diffopt+=vertical")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,9 +2,12 @@ local use = require('packer').use
 
 require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-  use 'neovim/nvim-lspconfig'
   use 'justinmk/vim-dirvish'
   use 'w0ng/vim-hybrid'
+
+  -- lsp
+  use "neovim/nvim-lspconfig"
+  use "williamboman/nvim-lsp-installer"
 
   -- fuzzy finder
   use 'junegunn/fzf'
@@ -29,6 +32,44 @@ vim.cmd([[
 ]])
 
 -- color
-vim.cmd("colorscheme hybrid")
-vim.cmd("set termguicolors")
+vim.cmd([[
+  colorscheme hybrid
+  set termguicolors
+]])
+
+if vim.api.nvim_win_get_option(0, "diff") then
+  vim.cmd("colorscheme hybrid")
+end
+
+-- lsp
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+end
+
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+    opts.on_attach = on_attach
+
+    server:setup(opts)
+end)
 

0530308827bb191e0b14ebfbbef9d253db881b0f
Junichi Sato <sat0b.3ee@gmail.com>
2022-03-01 14:53:22 +0900

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,9 +7,29 @@ require('packer').startup(function()
   use 'w0ng/vim-hybrid'
 
   -- lsp
-  use "neovim/nvim-lspconfig"
-  use "williamboman/nvim-lsp-installer"
-  use "folke/lsp-colors.nvim"
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/nvim-lsp-installer'},
+  
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+  
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+
+  -- format
+  use "mhartington/formatter.nvim"
 
   -- treesitter
   use {
@@ -25,9 +45,6 @@ require('packer').startup(function()
     end
   }
 
-  -- filer
-  use "justinmk/vim-dirvish"
-
   -- status line
   use {
     'nvim-lualine/lualine.nvim',
@@ -36,10 +53,6 @@ require('packer').startup(function()
 
   -- formatter
   use { "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } }
-
-  -- snipet
-  -- use "hrsh7th/vim-vsnip"
-  -- use "hrsh7th/vim-vsnip-integ"
 
   -- complete
   use "hrsh7th/nvim-cmp"
@@ -54,37 +67,17 @@ require('packer').startup(function()
   use { "junegunn/fzf", run = ":call fzf#install()" }
   use "junegunn/fzf.vim"
 
-  -- telescope
-  -- use {
-  --   'nvim-telescope/telescope.nvim',
-  --   requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-  -- }
-
-  -- tree
-  -- use {
-  --     'kyazdani42/nvim-tree.lua',
-  --     requires = {
-  --       'kyazdani42/nvim-web-devicons', -- optional, for file icon
-  --     },
-  --     config = function() require'nvim-tree'.setup {} end
-  -- }
-
   -- git
   use 'airblade/vim-gitgutter'
   use 'tpope/vim-fugitive'
 
-  -- github
-  -- use {
-  --   'pwntester/octo.nvim',
-  --   requires = {
-  --     'nvim-lua/plenary.nvim',
-  --     'nvim-telescope/telescope.nvim',
-  --     'kyazdani42/nvim-web-devicons',
-  --   },
-  --   config = function ()
-  --     require"octo".setup()
-  --   end
-  -- }
+  -- markdown
+  use 'godlygeek/tabular'
+  use 'preservim/vim-markdown'
+
+  -- nerdtree
+  use 'preservim/nerdtree'
+  use 'Xuyuanp/nerdtree-git-plugin'
 end)
 
 -- fzf
@@ -137,38 +130,8 @@ vim.cmd([[
   let g:vsnip_filetypes.typescriptreact = ['typescript']
 ]])
 
--- lsp
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-  local opts = { noremap=true, silent=true }
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-end
-
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-    opts.on_attach = on_attach
-    opts.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-    server:setup(opts)
-end)
+-- NERDTree
+vim.cmd("nnoremap <silent><C-n> :NERDTreeToggle<CR>")
 
 -- complete
 vim.opt.completeopt = "menu,menuone,noselect"
@@ -203,15 +166,10 @@ local nullls = require "null-ls" nullls.setup {
 }
 
 -- lsp
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+local lsp = require('lsp-zero')
 
-vim.diagnostic.config({
-  virtual_text = false
-})
+lsp.preset('recommended')
+lsp.setup()
 
 -- Show line diagnostics automatically in hover window
 vim.o.updatetime = 250
@@ -219,4 +177,8 @@ vim.cmd("autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {fo
 
 -- lualine
 require('lualine').setup()
+
+-- markdown
+vim.cmd("let g:vim_markdown_new_list_item_indent = 2")
+vim.cmd("let g:vim_markdown_folding_disabled = 1")
 
